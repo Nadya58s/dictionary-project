d1b7a41c368e1eed258fde6100ea3497ba4bd41a
Nadia Sylvester <126354130+Nadya58s@users.noreply.github.com>
2023-07-06 08:39:17 +0100

    Added phonetic

--- src/Meaning.js
+++ src/Meaning.js
@@ -4,7 +4,7 @@ import React from "react";
 import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
-  console.log(props.meaning);
+ 
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
